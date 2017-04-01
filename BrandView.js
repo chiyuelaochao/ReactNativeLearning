d5f90ca4afcd72f582b7ff8afc14d3fac6e5419b
Cai Wei <49383207@qq.com>
2017-04-02 01:09:53 +0800

    first commit

---
+++ BrandView.js
@@ -0,0 +1,49 @@
+/**
+ * Created by Cai Wei on 4/2/2017.
+ */
+import React, {Component} from 'react';
+import {
+    StyleSheet,
+    Text,
+    View,
+    Image
+} from 'react-native';
+
+let JsonData = require('./data.json');
+
+let brandWidth = require('Dimensions').get('window').width / 3;
+
+class BrandView extends Component {
+    renderBrands = ()=> {
+        return JsonData.data.map((item, i) => {
+            return <View key={`wrapper${i}`} style={styles.wrapperStyle}>
+                <Image source={require('./images/icon7.png')} style={styles.imageStyle}></Image>
+                <Text>{item.title}</Text>
+            </View>
+        });
+    }
+
+    render() {
+        return <View style={styles.container}>
+            {this.renderBrands() }
+        </View>;
+    }
+}
+
+const styles = StyleSheet.create({
+    container: {
+        flexDirection: 'row',
+        flexWrap: 'wrap'
+    },
+    wrapperStyle: {
+        flexDirection: 'column',
+        alignItems: 'center',
+        width: brandWidth,
+        height: 100
+    },
+    imageStyle: {
+        width: 80,
+        height: 80
+    }
+});
+module.exports = BrandView;
---
+++ index.android.js
@@ -0,0 +1,68 @@
+/**
+ * Sample React Native App
+ * Created by Cai Wei on 4/2/2017.
+ */
+import React from 'react';
+import {
+    AppRegistry,
+    StyleSheet,
+    Text,
+    View,
+    Image
+} from 'react-native';
+
+// get screen width
+let Dimensions = require('Dimensions');
+let screenWidth = Dimensions.get('window').width;
+let textWidth = screenWidth / 4;
+
+/*export default class first extends React.Component {
+ render() {
+ return <View style={styles.container}>
+ <Text style={[styles.textStyle,{backgroundColor:'#0f0'}]}>
+ text1
+ </Text>
+ <Text style={[styles.textStyle,{backgroundColor:'#00F'}]}>
+ text2
+ </Text>
+ <Text style={[styles.textStyle,{height:70}]}>
+ text3
+ </Text>
+ <Text style={[styles.textStyle,{height:70}]}>
+ text4
+ </Text>
+ <Text style={[styles.textStyle,{height:70}]}>
+ text5
+ </Text>
+ </View>
+ }
+ }
+
+ let styles = StyleSheet.create({
+ container: {
+ flexDirection: 'row',
+ backgroundColor: '#FF0',
+ flexWrap: 'wrap',
+ justifyContent:'flex-end',
+ alignItems: 'center'
+ },
+ textStyle: {
+ width: textWidth,
+ height: 40,
+ backgroundColor: '#F00',
+ margin: 10
+ // flex:1
+ }
+ })*/
+
+let BrandView = require('./BrandView');
+
+export default class first extends React.Component {
+    render() {
+        return <View>
+            <BrandView/>
+        </View>
+    }
+}
+
+AppRegistry.registerComponent('first', ()=>first);
